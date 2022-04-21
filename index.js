49914ff0b7f21096cb650918f7f01cdaa4abf0c2
sharkbyte <sgquin@gmail.com>
2022-04-20 17:05:25 -0700

    required generator.js, began template

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const generate = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    message: "What is the name of your application?",
+    name: 'name'
+  }, 
+  {
+    input: "select",
+    message: "Please select a license",
+    choices: [],
+    name: 'license'
+  }, 
+  {
+    message: "Describe your application in 3 sentences",
+    name: 'description'
+  }, 
+  {
+    message: 'What is your e-mail address',
+    name: 'email'
+  }, 
+  {
+    message: "What is your github username",
+    name: 'github'
+  }, 
+
+];
+
+function askUser(qArray) {
+  inquirer.prompt(
+    qArray
+  )
+  .then((answers) => {
+    generate.generateMarkdown(answers)
+  })
+}
+let generatedMD =  
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//
+function writeToFile(, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  askUser(questions)
+  generateMarkdown()
+}
 
 // Function call to initialize app
 init();
---
+++ template.md
@@ -0,0 +1,5 @@
+#${name}
+
+##${description}
+
+## Table of contents
